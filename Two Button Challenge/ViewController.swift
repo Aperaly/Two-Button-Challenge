0293614bf33cd70b593bc0cffea38aa29fd11675
Tsukasa Mita <tsukasa.mita@gmail.com>
2022-01-27 14:16:25 +0100

    Completed Two Button Pressed with textColour and textAlignement

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func youAreAwsomeButton(_ sender: UIButton) {
+        print("Show Message clicked")
+        messageLabel.text = "You Are Awsome!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left   
+    }
+    
+    @IBAction func showAntoherMessageButton(_ sender: UIButton) {
+        print("Show Another clicked")
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = .right
+    }
+    
 }
 
